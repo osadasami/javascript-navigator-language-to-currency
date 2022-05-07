166fadfec75ac4f145c06c82f1b8cea2a0fd7e6b
osadasami <osadasami@gmail.com>
2022-05-07 11:44:04 +0500

    add return type to function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { currencies } from './currencies'
+import { currencies, currency } from './currencies'
 
-export function localeToCurr(locale: string) {
+export function localeToCurr(locale: string): currency {
   const code = currencies.find((item) => item.locale === locale)
   if (!code) throw new TypeError('locale is invalid')
   return code
